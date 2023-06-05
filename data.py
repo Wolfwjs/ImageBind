0a8bd3f552b47b22edcedbfdab702b6b83df7a1d
wolfwjs <976330548@qq.com>
2023-06-05 13:53:37 +0800

    modify data.py

--- data.py
+++ data.py
@@ -16,7 +16,9 @@ from pytorchvideo import transforms as pv_transforms
 from pytorchvideo.data.clip_sampling import ConstantClipsPerVideoSampler
 from pytorchvideo.data.encoded_video import EncodedVideo
 from torchvision import transforms
-from torchvision.transforms._transforms_video import NormalizeVideo
+# from torchvision.transforms._transforms_video import NormalizeVideo
+from torchvision.transforms.functional import normalize
+# from torchvideo
 
 from models.multimodal_preprocessors import SimpleTokenizer
 
@@ -296,7 +298,7 @@ def load_and_transform_video_data(
     video_transform = transforms.Compose(
         [
             pv_transforms.ShortSideScale(224),
-            NormalizeVideo(
+            normalize(
                 mean=(0.48145466, 0.4578275, 0.40821073),
                 std=(0.26862954, 0.26130258, 0.27577711),
             ),
